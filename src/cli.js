16685ec5a176ae9848f3b0e4da72ba8d81549f13
Ayush Gupta <AyushG3112@gmail.com>
2020-02-05 11:51:51 +0530

    exitcodes

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,27 @@
 require = require("esm")(module /*, options*/);
 import argumentParser from "./argumentParser";
-import process from "./processor";
+import processor from "./processor";
 import getHandler from "./handlers";
 
 export async function cli(args) {
-  const cliArgs = argumentParser.build(args);
-  const errors = cliArgs.validate();
-  if (!errors.length) {
-    const result = process(...cliArgs.args.files, cliArgs);
-    await (getHandler(cliArgs.args).handle(result));
-  } else {
-    throw new Error(errors[0]);
+  let exitCode = 1;
+  try {
+    const cliArgs = argumentParser.build(args);
+    const errors = cliArgs.validate();
+    if (!errors.length) {
+      const result = processor(...cliArgs.args.files, cliArgs);
+      await getHandler(cliArgs.args).handle(result);
+      exitCode = 3;
+      throw new Error(
+        `Some differences found. Details have been sent to ${cliArgs.args
+          .outFile || "console"}.`
+      );
+    } else {
+      exitCode = 4;
+      throw new Error(errors[0]);
+    }
+  } catch (e) {
+    console.error(e);
+    process.exit(exitCode);
   }
 }
