914158bcb2bde24a7e49514acff2ea65908e3155
Ayush Gupta <AyushG3112@gmail.com>
2020-02-05 11:37:24 +0530

    getType changes

--- src/utils/getType.js
+++ src/utils/getType.js
@@ -1,3 +1,7 @@
 export default function getType(input) {
-  return Object.prototype.toString.call(input);
+  return Object.prototype.toString
+    .call(input)
+    .replace(/^\[object /, "")
+    .replace(/\]/, "")
+    .toLowerCase();
 }
